d5c01ebcd777dea2e58a64039cc800958bc65c88
gambi <stephane.gambini@isen.yncrea.fr>
2022-03-15 18:31:48 +0100

    requete post send and receive commit

--- app/src/main/java/fr/isen/gambini/androiderestaurant/CategoryActivity.kt
+++ app/src/main/java/fr/isen/gambini/androiderestaurant/CategoryActivity.kt
@@ -3,11 +3,17 @@ package fr.isen.gambini.androiderestaurant
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import fr.isen.gambini.androiderestaurant.databinding.ActivityCategoryBinding
+import org.json.JSONObject
 
 class CategoryActivity : AppCompatActivity() {
 
@@ -29,5 +35,25 @@ class CategoryActivity : AppCompatActivity() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
      }
+        request_Post()
     }
+    private fun request_Post(){
+        val queue = Volley.newRequestQueue(this)
+        val url = "http://test.api.catering.bluecodegames.com/menu"
+
+        val requestBody = JSONObject("{\"id_shop\":1}")
+        val req =
+            object : JsonObjectRequest(Method.POST, url,requestBody,
+                Response.Listener { response ->
+                    Log.d("API00", "json => $response")
+                },
+                Response.ErrorListener { error ->
+                    Log.d("API00", "error => $error")
+                }
+            ){
+            }
+        queue.add(req)
+    }
+
 }
+
